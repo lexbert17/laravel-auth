ad04d5cdf3638089eba16dc5c5e34c5a0e79d0e8
lexbert17 <lexbertlazo@gmail.com>
2024-01-24 00:15:30 +0100

    add: creazione pagina index

---
+++ resources/views/Admin/projects/index.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="container mt-5">
+    <h2>Lista dei post</h2>
+
+    <div class="text-end">
+      <a class="btn btn-primary" href="{{ route('admin.project.create') }}">Crea un nuovo post</a>
+    </div>
+
+    <table class="table table-striped mt-5">
+        <thead>
+          <tr>
+            <th scope="col">id</th>
+            <th scope="col">Titolo</th>
+            <th scope="col">Data</th>
+            <th scope="col">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+            @foreach ($projects as $project)
+                <tr>
+                    <th scope="row">{{ $project->id }}</th>
+                    <td>{{ $project->title }}</td>
+                    <td>{{ $project->created_at }}</td>
+                    <td>
+                      <a class="btn btn-success" href="{{ route('admin.project.show', ['project' => $project->slug]) }}">Dettagli</a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+      </table>
+</div>
+@endsection
